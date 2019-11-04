5a726de3b559ac9e9033f42d45a40713827e59fe
yuanming <yuanming@dian.so>
2019-11-04 10:36:35 +0800

    init

---
+++ src/main/java/so/dian/pisces/PiscesApplication.java
@@ -0,0 +1,13 @@
+package so.dian.pisces;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PiscesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PiscesApplication.class, args);
+	}
+
+}
