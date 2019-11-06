f9e8f7b3d1eec39c3ecacd0e43e813e9e0675367
yuanming <yuanming@dian.so>
2019-11-06 15:38:53 +0800

    spring & log

--- src/main/java/so/dian/pisces/PiscesApplication.java
+++ src/main/java/so/dian/pisces/PiscesApplication.java
@@ -2,8 +2,20 @@ package so.dian.pisces;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableFeignClients
+@EnableEurekaClient
+@ServletComponentScan
+@EnableHystrix
+@EnableCircuitBreaker
+@EnableScheduling
 public class PiscesApplication {
 
 	public static void main(String[] args) {
