1fd172c35c6a4971aea3310c519ac7333a0d35bc
yuanming <yuanming@dian.so>
2019-11-11 11:38:26 +0800

    log

--- src/main/java/so/dian/pisces/job/SeerJob.java
+++ src/main/java/so/dian/pisces/job/SeerJob.java
@@ -1,7 +1,10 @@
 package so.dian.pisces.job;
 
+import com.alibaba.fastjson.JSON;
 import com.dangdang.ddframe.job.api.ShardingContext;
 import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import so.dian.pisces.manager.SeerManager;
 
@@ -11,12 +14,14 @@ import so.dian.pisces.manager.SeerManager;
  * Description
  */
 public class SeerJob implements SimpleJob {
+    private Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
     private SeerManager seerManager;
 
     @Override
     public void execute(ShardingContext shardingContext) {
+        log.info("SeerJob start... shardingContext:{}", JSON.toJSONString(shardingContext));
         seerManager.predict();
     }
 }
